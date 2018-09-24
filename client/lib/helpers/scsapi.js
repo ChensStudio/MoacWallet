274501cedbdfda530698c594cc3cea83ee1ebbf2
Yang Chen <young.y.chen@gmail.com>
2018-09-24 09:39:44 -0700

    add new functions for scsApi

--- client/lib/helpers/scsapi.js
+++ client/lib/helpers/scsapi.js
@@ -18,22 +18,14 @@ scsApi = {initFlag : false};
 
 scsApi.init = function () {
     if (!scsApi.initFlag) {
-    scsApi.options = {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-        }
-    };
-    // scsApi.url = 'http://127.0.0.1:1337/rpc'
-    // scsApi.url = 'http://139.198.126.104:8546/rpc'
-    // scsApi.url = 'http://35.196.114.202:50068/rpc'
-    // scsApi.contractAddress = '0xf6a97597540165b9accd3837adfb7d1e77397bc1'
-    // scsApi.abi = [{"constant":false,"inputs":[],"name":"list","outputs":[{"name":"count","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"fileHash","type":"string"}],"name":"read","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"fileHash","type":"string"}],"name":"remove","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"fileHashes","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"s1","type":"string"},{"name":"s2","type":"string"}],"name":"compareStringsbyBytes","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"fileHash","type":"string"}],"name":"write","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
-    // scsApi.sender = '0x18e926ad1821e38597368b606be81de580f46686'
-    // scsApi.initFlag = true;
+        scsApi.options = {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            }
+        };
     }
-
 };
   
 scsApi.request = function(bodyJSON, rpcaddr, rpcport, sender, subChainAddr, callback) {
@@ -55,5 +47,27 @@ scsApi.getNonce = function(rpcaddr, rpcport, sender, subChainAddr, callback) {
         sender = scsApi.sender;
     }
     var bodyJSON = { "jsonrpc": "2.0", "id": 0, "method": "ScsRPCMethod.GetNonce", "params": {"Sender": sender, "SubChainAddr": subChainAddr} };
+    return scsApi.request(bodyJSON, rpcaddr, rpcport, sender, subChainAddr, callback);
+};
+
+scsApi.anyCall = function(rpcaddr, rpcport, sender, subChainAddr, funcName, callback) {
+    if (!subChainAddr) {
+        subChainAddr = scsApi.contractAddress;
+    }
+    if (!sender) {
+        sender = scsApi.sender;
+    }
+    var bodyJSON = { "jsonrpc": "2.0", "id": 0, "method": "ScsRPCMethod.AnyCall", "params": {"Sender": sender, "SubChainAddr": subChainAddr, "Params": [funcName]} };
+    return scsApi.request(bodyJSON, rpcaddr, rpcport, sender, subChainAddr, callback);
+};
+
+scsApi.setDappAbi = function(rpcaddr, rpcport, sender, subChainAddr, abi, callback) {
+    if (!subChainAddr) {
+        subChainAddr = scsApi.contractAddress;
+    }
+    if (!sender) {
+        sender = scsApi.sender;
+    }
+    var bodyJSON = { "jsonrpc": "2.0", "id": 0, "method": "ScsRPCMethod.SetDappAbi", "params": {"Sender": sender, "Data": abi, "SubChainAddr": subChainAddr}};
     return scsApi.request(bodyJSON, rpcaddr, rpcport, sender, subChainAddr, callback);
 };
