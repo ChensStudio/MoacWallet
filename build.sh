f12c8b7a885c6278a15b6a870ebb88f165d86da8
Yang Chen <young.y.chen@gmail.com>
2018-11-09 16:54:19 -0800

    update solc in build script to version 0.4.24

--- build.sh
+++ build.sh
@@ -19,8 +19,9 @@ done
 
 if test ${target} != "exit"
 then
-    curl -o ./public/solc-bin.js https://rawgit.com/ethereum/solc-bin/gh-pages/bin/soljson-latest.js
+    #curl -o ./public/solc-bin.js https://rawgit.com/ethereum/solc-bin/gh-pages/bin/soljson-latest.js
     #curl -o ./public/solc-bin.js https://rawgit.com/ethereum/solc-bin/gh-pages/bin/soljson-v0.4.21+commit.dfe3193c.js
+    curl -o ./public/solc-bin.js https://rawgit.com/ethereum/solc-bin/gh-pages/bin/soljson-v0.4.24+commit.e67f0147.js
     meteor build --architecture os.linux.x86_64  ../build/MoacWallet
     scp -i /Users/ychen/innowells/pem/gcp ../build/MoacWallet/MoacWallet.tar.gz ${target}
 fi
